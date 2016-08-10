ed4c45721de680c4ca3a243f35267d9543d43f8f
Russeii <rratcliffe57@gmail.com>
2016-08-10 06:19:29 -0400

    commit for work

--- engine/scan.py
+++ engine/scan.py
@@ -7,16 +7,22 @@ class findRoomates():
 
     def find_roomates(self):
         Roos="Russell is not home"
+        Grace="Grace is not in my appartment"
         subprocess.call("echo milk | sudo -S arp-scan --localnet  > allout.txt 2>&1", shell=True)
         if '64:bc:0c:65:43:22' in open('allout.txt').read():
             Roos="Russell is home"
+
+
+            if '12:b3:b7:d2:e2:3d' in open('allout.txt').read():
+                Grace="Grace is at my appartment"
         AJ="AJ is not Home"
         Orion="Orion is not home"
 
         roomates = [
         {"room":Roos,},
         {"room":AJ,},
-        {"room":Orion,}
+        {"room":Orion,},
+        {"room":Grace,}
         ]
         roomates=json.dumps(roomates)
         print roomates
