28d2c558d97b3835d16a9e0851fd5c734697bd6a
Russeii <rratcliffe57@gmail.com>
2016-08-09 23:33:58 -0400

    just need frontend and autoresfresh

--- engine/scan.py
+++ engine/scan.py
@@ -6,16 +6,17 @@ class findRoomates():
         print "created findRoomates object"
 
     def find_roomates(self):
+        Roos="Russell is not home"
         subprocess.call("echo milk | sudo -S arp-scan --localnet  > allout.txt 2>&1", shell=True)
         if '64:bc:0c:65:43:22' in open('allout.txt').read():
-            Roos="Russell is Home"
-        AJ="donger"
-        Orion="p"
+            Roos="Russell is home"
+        AJ="AJ is not Home"
+        Orion="Orion is not home"
 
         roomates = [
-        {"home":Roos,},
-        {"home":AJ,},
-        {"home":Orion,}
+        {"room":Roos,},
+        {"room":AJ,},
+        {"room":Orion,}
         ]
         roomates=json.dumps(roomates)
         print roomates
